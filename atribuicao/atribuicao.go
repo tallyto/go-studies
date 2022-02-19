d0b7085b67d8d6022ddea27babbb165e662f38c9
tallyto <rodrigues.tallyto@hotmail.com>
2022-02-19 16:26:10 -0300

    atribuicao

---
+++ atribuicao/atribuicao.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b byte = 3
+	fmt.Println(b)
+
+	i := 3 // tipo inferido pelo compilador
+	i += 3 // i = i + 3
+	i -= 3 // i = i - 3
+	i *= 2 // i = i * 3
+	i /= 3 // i = i / 3
+	i %= 2 // i = i % 3
+	fmt.Println(i)
+
+	x, y := 3, 4
+	fmt.Println(x, y)
+
+	x, y = y, x
+	fmt.Println(x, y)
+}
