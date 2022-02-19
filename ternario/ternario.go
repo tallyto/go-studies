c189478a0cb94ca979cd3e17107a06abbfc35933
tallyto <rodrigues.tallyto@hotmail.com>
2022-02-19 17:07:22 -0300

    nao existe operador ternario

---
+++ ternario/ternario.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Não existe operador ternário em Go.
+
+func obterResultado(nota float64) string {
+	if nota >= 6 {
+		return "Aprovado"
+	}
+	return "Reprovado"
+
+}
+
+func main() {
+	fmt.Println(obterResultado(6.2))
+	fmt.Println(obterResultado(4.9))
+}
